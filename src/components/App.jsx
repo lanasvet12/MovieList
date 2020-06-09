571bbf846ae6ee6ab3257b7bece026d664a667a5
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-06-09 15:33:11 +0300

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { moviesData } from "../moviesData";
+
+class App extends Component {
+  render() {
+    return <div>React Warriors Stage 1</div>;
+  }
+}
+
+export default App;
